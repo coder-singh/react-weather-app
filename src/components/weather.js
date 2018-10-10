81b537512c8c383d02fa2fc0b99f8f8f15097400
Satyendra <Satyendra199612@gmail.com>
2018-10-10 12:11:55 +0530

    Added days to forecasted weather cards
    - Cleared png images
    - Added days to 4 future weather cards
    - Changed display with image by using css-flexbox

--- src/components/weather.js
+++ src/components/weather.js
@@ -2,16 +2,32 @@ import React from 'react';
 
 class Weather extends React.Component{
     render(){
-        console.log(this.props.main)
         var str = "images/"+ this.props.main + ".png";
+        var date = new Date(this.props.date);
+        
+        var weekday = new Array(7);
+        weekday[0] =  "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+
+        var day = weekday[date.getDay()];
         return (
-        <div className="card">
-            {this.props.main && <p><img src = {str} />{this.props.main}</p>}
-            {this.props.temperature && <p>Temperature: {(this.props.temperature-273)|0} C</p>}
-            {this.props.humidity && <p>Humidity: {this.props.humidity}%</p>}
-            {this.props.description && <p>Atmosphere: {this.props.description}</p>}
-            {this.props.date && <p>Date: {this.props.date}</p>}
-            {this.props.error && <p>{this.props.error}</p>}
+        <div className="card" key={this.props.date}>
+            {this.props.date && <div className="date">
+                {day}
+            </div>}
+            <div className="info">
+                {this.props.main && <div class="img"><img src = {str} /><span>{this.props.main}</span></div>}
+                {this.props.temperature && <p>Temperature: {(this.props.temperature-273)|0} C</p>}
+                {this.props.humidity && <p>Humidity: {this.props.humidity}%</p>}
+                {this.props.description && <p>Atmosphere: {this.props.description}</p>}
+                {this.props.date && <p>Date: {this.props.date}</p>}
+                {this.props.error && <p>{this.props.error}</p>}
+            </div>
         </div>
         )
     }
--- src/components/weatherToday.js
+++ src/components/weatherToday.js
@@ -2,14 +2,26 @@ import React from 'react';
 
 class WeatherToday extends React.Component{
     render(){
+        var date = new Date(this.props.date);
+        
+        var weekday = new Array(7);
+        weekday[0] =  "Sunday";
+        weekday[1] = "Monday";
+        weekday[2] = "Tuesday";
+        weekday[3] = "Wednesday";
+        weekday[4] = "Thursday";
+        weekday[5] = "Friday";
+        weekday[6] = "Saturday";
+
+        var day = weekday[date.getDay()];
         return (
         <div className="main-card">
             {this.props.temperature && <div id="temperature">
-                <img src = "images/clear.png" width="60px"></img>
+                <img src = "images/clear.png"></img>
                 <span> {(this.props.temperature-273).toFixed(0)}</span>
             </div>}
             <div id="description">{this.props.description}</div>
-            {this.props.humidity && <div id="details">Humidity: {this.props.humidity}% Date: {this.props.date}</div>}
+            {this.props.humidity && <div id="details">Humidity: {this.props.humidity}%<br />Date: {this.props.date}</div>}
         </div>
         )
     }
